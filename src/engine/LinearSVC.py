42115e3ef3778ece03ff28bf25d8c91081551c2e
EEstevanell <e.estevanell@estudiantes.matcom.uh.cu>
2019-12-27 12:13:20 -0500

    Base Classifier implemented

---
+++ src/engine/LinearSVC.py
@@ -0,0 +1,16 @@
+from base import BaseAdvertiseClassifier, movie_reviews_test
+from sklearn.svm import LinearSVC
+
+class LinearSVClassifier(BaseAdvertiseClassifier):
+    """
+    Multiclass classifier based on Support Vector Machine
+    """
+    def __init__(self, language = 'english', vectorizer = 'tf_vectorizer', penalty = 'l2'):
+        super(type(self), self).__init__(language = language, vectorizer = vectorizer)
+        self.penalty = penalty
+        self.classifier = LinearSVC()
+
+    def get_params(self, deep = True):
+        params = {}
+        params.update(super().get_params(deep))
+        return params
---
+++ src/engine/MultinomialNB.py
@@ -0,0 +1,16 @@
+from base import BaseAdvertiseClassifier, movie_reviews_test
+from sklearn.naive_bayes import MultinomialNB
+
+class MultinomialNBClassifier(BaseAdvertiseClassifier):
+    """
+    Multiclass classifier based on Multinomial Naive Bayes
+    """
+    def __init__(self, language = 'english', vectorizer = 'tf_vectorizer', alpha = 1.0):
+        super(type(self), self).__init__(language = language, vectorizer = vectorizer)
+        self.alpha = alpha
+        self.classifier = MultinomialNB(alpha = alpha)
+
+    def get_params(self, deep = True):
+        params = {'alpha': self.alpha}
+        params.update(super().get_params(deep))
+        return params
