65cf2d76d9b0df689fd2d4724473603ffbfa7c53
EEstevanell <e.estevanell@estudiantes.matcom.uh.cu>
2019-12-27 12:22:15 -0500

    Create LogisticRegression.py
    Implemented `LogisticRegressionClassifier`

---
+++ src/engine/LogisticRegression.py
@@ -0,0 +1,16 @@
+from base import BaseAdvertiseClassifier, movie_reviews_test
+from sklearn.linear_model import LogisticRegression
+
+class LogisticRegressionClassifier(BaseAdvertiseClassifier):
+    """
+    Multiclass classifier based on Logistic regression
+    """
+    def __init__(self, language = 'english', vectorizer = 'tf_vectorizer', penalty = 'l2'):
+        super(type(self), self).__init__(language = language, vectorizer = vectorizer)
+        self.penalty = penalty
+        self.classifier = LogisticRegression(multi_class = 'multinomial', penalty = penalty, solver = 'newton-cg')
+
+    def get_params(self, deep = True):
+        params = {'penalty': self.penalty}
+        params.update(super().get_params(deep))
+        return params
